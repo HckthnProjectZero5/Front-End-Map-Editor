b543276e4b4e3f098d68d2fc697dc9e457cc0d65
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-27 14:43:59 -0400

    Object positions saving as integer-based coordinates

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,25 @@
 var obstaclePosition = [];
 
+var Block = function(x){
+  this.length = 1;
+  this.width = 1;
+  this.x = '';
+  this.y = '';
+};
+
 
 
 
 $('.brick').draggable({
 
   snap: "#map div", snapMode: "inner", helper: 'clone',
-  stop: function( event, position ) {
-    console.log(position);
-
+  stop: function( event, ui ) {
+    var newBlock = new Block();
+    var pos = ui.position;
+    console.log(pos);
+    newBlock.x = (pos.left - 352)/30;
+    newBlock.y = (pos.top - 82)/30;
+    console.log(newBlock);
  }
 
 });
