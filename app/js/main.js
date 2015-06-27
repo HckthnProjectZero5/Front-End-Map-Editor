cbac2206f4bea371c134b892f7e2d372ef6e0e3e
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-27 16:43:29 -0400

    Wrote methods to save an entire map object

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
+// Establishes an array of objects on the map
 var obstaclePosition = [];
 
+// Constructor for individual objects on the map
 var Block = function(x){
   this.length = 1;
   this.width = 1;
@@ -7,19 +9,29 @@ var Block = function(x){
   this.y = '';
 };
 
+// Constructor for entire map object
+var Map = function(x){
+  this.name = $('#title').val();
+  this.budget = $('#budget').val();
+  this.map = obstaclePosition;
+};
 
+// Makes post request to save map on in the database
+var postMap = function(){
+
+};
+
+// Drag and drop functionality for building blocks
 $('.brick').draggable({
-
   snap: "#map div", snapMode: "inner", helper: 'clone',
   stop: function( event, ui ) {
     var newBlock = new Block();
     var pos = ui.position;
-    console.log(pos);
     newBlock.x = (pos.left - 350)/30;
     newBlock.y = (pos.top - 100)/30;
     console.log(newBlock);
+    obstaclePosition.push(newBlock);
  }
-
 });
 
 $('#map div').each(function() {
@@ -29,4 +41,10 @@ $('#map div').each(function() {
       $(this).append($(ui.draggable).clone());
     }
   });
+});
+
+// Click event creating map object and sending it to server
+$('#submit').click(function(){
+  Map();
+  postMap();
 });
