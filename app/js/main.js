79a4ba60dadf261c38adb295733f0c8f68c77a11
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-27 11:56:36 -0400

    Added code to make draggable, droppable

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$('.brick').draggable();
+
+
+
+$('div', '#map').each(function() {
+    var $div = $(this);
+    $div.droppable({
+        drop: function() {
+            $('.brick').addClass('dropped').
+            css({
+                top: $div.offset().top,
+                left: $div.offset().left
+            });
+            $('#map').addClass('focus');
+        }
+    });
+});
