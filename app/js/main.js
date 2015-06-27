aa9bd71bff80b10af12088299e5c6e4f4420f22f
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-27 13:25:33 -0400

    Scaled grid up to size 20 x 20

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,19 @@
 
 $('.brick').draggable({
- snap: "#map div", snapMode: "inner", helper: 'clone'
-});
+ snap: "#map div", snapMode: "inner", helper: 'clone',
+ stop: function( event, ui ) {
 
+ }
+});
 
 
 $('#map div').each(function() {
   var $div = $(this);
   $div.droppable({
     drop: function() {
-      // $(this).addClass('dropped').
-      // css({
-      //     top: $div.offset().top,
-      //     left: $div.offset().left
-      // });
-      // $('#map').addClass('focus');
+
         }
     });
 });
+
 
