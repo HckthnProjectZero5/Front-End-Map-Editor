a90dc61427926f501ef03c01f8f46f57a3725fb3
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-28 12:48:44 -0400

    Added success dialog box that reloads page on OK

--- app/js/main.js
+++ app/js/main.js
@@ -16,14 +16,13 @@ var Block = function(x){
 // Makes post request to save map on in the database
 
 var url = 'https://fierce-wave-2814.herokuapp.com/levels/new';
-
 var postMap = function(map){
   $.ajax({
     url: url,
     type: 'POST',
     data: map
     }).done( function (data) {
-      console.log(data);
+      $( "#dialog" ).dialog( "open" );
     });
   };
 
@@ -37,21 +36,7 @@ $('.brick, .tape, .computer').draggable({
     console.log(ui.position);
     newBlock.x = (pos.left - 575)/28;
     newBlock.y = (pos.top - 25)/28;
-    console.log(newBlock);
     obstaclePosition.push(newBlock);
-  },
-  start: function( event, ui) {
-    var pos = ui.position;
-    console.log(pos);
-    x = (pos.left - 575)/28;
-    y = (pos.top - 25)/28;
-    var newArray = [];
-    obstaclePosition.map(function(item){
-      if (item.x !== x || item.y !== y){
-        newArray.push(item);
-        obstaclePosition = newArray;
-      }
-    });
   }
 });
 
@@ -60,6 +45,7 @@ $('#map div').each(function() {
   $div.droppable({
     hoverClass: 'highlight',
     drop: function (event, ui) {
+
       // Re-enable draggability of dropped block
       if ($(ui.draggable).hasClass('copied')) {
         return;
@@ -73,15 +59,27 @@ $('#map div').each(function() {
       var drag_id = $(ui.draggable).attr("id");
       var targetElem = $(this).attr("id");
       deleteMe = true;
-    }
+    },
+    start: function( event, ui) {
+      var pos = ui.position;
+      console.log(pos);
+      x = (pos.left - 575)/28;
+      y = (pos.top - 25)/28;
+      var newArray = [];
+      obstaclePosition.map(function(item){
+        if (item.x !== x || item.y !== y){
+          newArray.push(item);
+          obstaclePosition = newArray;
+      }
+    });
+  }
   });
 });
 
 
 // Click event creating map object and sending it to server
 $('#submit').on('click', function(e) {
-  // e.preventDefault();
-
+  e.preventDefault();
   var name = $('#title').val();
   var budget = $('#budget').val();
   var map = {
@@ -91,11 +89,7 @@ $('#submit').on('click', function(e) {
     map: obstaclePosition
     }
   };
-
   postMap(map);
-
-
-  console.log(map);
 });
 
 
@@ -105,4 +99,12 @@ $('.recycleBin').droppable({
     ui.helper.hide('explode');
     $(ui.draggable).remove();
   }
+});
+
+$("#dialog").dialog({ autoOpen: false });
+
+
+//Click event to clear map creation notification
+$('#reset').on('click', function(e) {
+  location.reload();
 });
--- app/js/templates.js
+++ app/js/templates.js
@@ -1,5 +1,15 @@
-this["template"] = this["template"] || {};
-this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["hbs"] = this["hbs"] || {};
+this["hbs"]["dialog"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1, alias1=this.lambda, alias2=this.escapeExpression;
+
+  return "<span>You made a new map!</span>\n\n<span>Title: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.map : depth0)) != null ? stack1.title : stack1), depth0))
+    + "</span>\n\n<span>Budget: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.map : depth0)) != null ? stack1.budget : stack1), depth0))
+    + "</span>\n";
+},"useData":true});
+this["hbs"] = this["hbs"] || {};
+this["hbs"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
   return "<h1>\n  "
